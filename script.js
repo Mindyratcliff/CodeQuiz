cb04466c25442cd949f7803a4e72d2bb42c4f15f
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-10-31 16:42:38 -0400

    Functioning timer in JS. Countdown from 60 seconds

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 //Grab a reference to the body of the document
 
 var body = document.body;
+var quizBody = document.getElementById("quizBody");
+var startQuiz = document.querySelector("#start");
+var welcomeText = document.createElement("p");
+var timeDisplay = document.getElementById("time");
+var questions = document.querySelectorAll(".form-check")
 
 
 //Keep score 
@@ -9,32 +14,41 @@ var userScore = 0;
 
 //Keep time
 
-var timeEl = document.querySelector(".time");
+
 var secondsLeft = 60;
 
 function setTime() {
     var timeInterval = setInterval( function() {
         secondsLeft --;
-        timeEl.textContent = "Time left: " + secondsLeft;
+        timeDisplay.textContent = "Time left: " + secondsLeft;
 
         if (secondsLeft === 0) {
             clearInterval(timeInterval);
-            
+            alert("Time is up!");
         }
     }
 , 1000);}
 
 
-
 //Create the Welcome page of the quiz
 
+welcomeText.innerHTML = "Click the start button to begin the quiz! Answer all questions before time is up!";
+
+quizBody.appendChild(welcomeText);
 
 
 
 
 //Make a start button to begin the quiz
 
-var generateBtn = document.querySelector("#Start");
+startQuiz.addEventListener('click', quizRunning);
+
+function quizRunning () {
+
+    quizBody.removeChild(welcomeText);
+    
+    setTime();
+}
 
 //Time decreases when a question is answered incorrectly
 
