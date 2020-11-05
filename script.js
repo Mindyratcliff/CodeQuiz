a896702f8c7bbca7dea3d19ab81f49b5ee5ec413
Mindy Ratcliff <mindy.ratcliff@gmail.com>
2020-11-05 13:48:54 -0500

    Changed from radios to buttons for easier navigation; updated nextQuestion function

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ var startQuiz = document.querySelector("#start");
 var welcomeText = document.createElement("p");
 var timeDisplay = document.getElementById("time");
 var nameInput = document.createElement("input");
-var submit = document.querySelector("#submit");
-var radioEl1 = document.querySelector("radioAnswer1");
-var radioEl2 = document.querySelector("radioAnswer2");
-var radioEl3 = document.querySelector("radioAnswer3");
-var radioEl4 = document.querySelector("radioAnswer4");
+var buttonEl1 = document.getElementById("button1");
+var buttonEl2 = document.getElementById("button2");
+var buttonEl3 = document.getElementById("button3");
+var buttonEl4 = document.getElementById("button4");
+var currentQuestion = 1;
 
 var userScore = 0;
 
@@ -26,31 +26,36 @@ var questionBank = {
 };
 
 var answerBank = {
-  answerSet1: [
-    "In the footer",
-    "In the body",
-    "In the head",
-    "Anywhere in the HTML",
-  ],
-  answerSet2: ["CAMELCASE", "camelcase", "cAmElCaSe", "camelCase"],
-  answerSet3: [
-    "The light that shines from your webcam",
-    "An unordered list",
-    "A collection of values in JavaScript",
-    "The return value of a confirm",
-  ],
-  answerSet4: [
-    "A counter in the for loop that counts the number of passes",
-    "An alert",
-    "A 3D element",
-    "A return value",
-  ],
-  answerSet5: [
-    "Key, value, storage, local",
-    "Margin, pudding, taco, element",
-    "Paragraph, background, body, head",
-    "Content, padding, border, margin",
-  ],
+  answerSet1: {
+    "In the footer": 0,
+    "In the body": 0,
+    "In the head": 1,
+    "Anywhere in the HTML": 0,
+  },
+  answerSet2: {
+    CAMELCASE: 0,
+    camelcase: 0,
+    cAmElCaSe: 0,
+    camelCase: 1,
+  },
+  answerSet3: {
+    "The light that shines from your webcam": 0,
+    "An unordered list": 0,
+    "A collection of values in JavaScript": 1,
+    "The return value of a confirm": 0,
+  },
+  answerSet4: {
+    "A counter in the for loop that counts the number of passes": 1,
+    "An alert": 0,
+    "A 3D element": 0,
+    "A return value": 0,
+  },
+  answerSet5: {
+    "Key, value, storage, local": 0,
+    "Margin, pudding, taco, element": 0,
+    "Paragraph, background, body, head": 0,
+    "Content, padding, border, margin": 1,
+  },
 };
 
 //Keep time
@@ -72,10 +77,10 @@ function setTime() {
 //Function to generate questions
 
 function displayQuestions(possibleQuestion) {
-    var question = document.createElement("h3");
-    question.textContent = questionBank[possibleQuestion];
-    questionBody.appendChild(question);
-  }
+  var question = document.createElement("h3");
+  question.textContent = questionBank[possibleQuestion];
+  questionBody.appendChild(question);
+}
 
 //Create the Welcome page of the quiz
 
@@ -86,68 +91,66 @@ quizBody.appendChild(welcomeText);
 
 //Make a start button to begin the quiz
 function quizRunning() {
-    quizBody.removeChild(welcomeText);
-    setTime();
-    displayQuestions("question1");
-    displayAnswers("answerSet1");
-  }
+  quizBody.removeChild(welcomeText);
+  setTime();
+  displayQuestions("question1");
+  displayAnswers("answerSet1");
+  currentQuestion++;
+}
 
 startQuiz.addEventListener("click", quizRunning);
-
 
 //Function to generate answers
 
 function displayAnswers(possibleAnswers) {
-  var option1 = document.getElementById("radioLabel1");
-  var option2 = document.getElementById("radioLabel2");
-  var option3 = document.getElementById("radioLabel3");
-  var option4 = document.getElementById("radioLabel4");
-  option1.textContent = answerBank[possibleAnswers][0];
-  option2.textContent = answerBank[possibleAnswers][1];
-  option3.textContent = answerBank[possibleAnswers][2];
-  option4.textContent = answerBank[possibleAnswers][3];
-  quizBody.appendChild(submit);
+  //Extract answers for use in function
+  var answerKeys = Object.keys(answerBank[possibleAnswers]);
+
+  //Pull answers from object list
+  buttonEl1.textContent = answerKeys[0];
+  buttonEl2.textContent = answerKeys[1];
+  buttonEl3.textContent = answerKeys[2];
+  buttonEl4.textContent = answerKeys[3];
+
+  //Add event listeners to each of the buttons
+
+  buttonEl1.addEventListener("click", nextQuestion);
+  buttonEl2.addEventListener("click", nextQuestion);
+  buttonEl3.addEventListener("click", nextQuestion);
+  buttonEl4.addEventListener("click", nextQuestion);
+  
 }
 
-//Time decreases when a question is answered incorrectly
+function nextQuestion(clickEvent) {
+  userScore = userScore + (answerBank["answerSet" + currentQuestion][clickEvent.target.textContent]);
+  localStorage.setItem("User Score", userScore);
+  //Display the next question and answer set
+    displayQuestions("question" + currentQuestion);
+    displayAnswers("answerSet" + currentQuestion);
+    currentQuestion++;
+   if (currentQuestion === 5){
+    //Collect and display user initials and high score
+    var userInput = document.createElement("p");
+    userInput.setAttribute("class", "result");
+    nameInput.setAttribute("type", "text");
+    nameInput.textContent = "Enter your initials";
+    quizBody.appendChild(nameInput);
+    nameInput.addEventListener("input", e)
 
-function nextQuestion (prevQuest, nextQuest) {
-    quizBody.classList.remove(prevQuest);
-    quizBody.classList.add(nextQuest);
+    //Generate a submit button to store user data 
+    var submit = document.createElement("button");
+    submit.setAttribute("type", "submit");
+    submit.textContent = "Submit";
+    quizBody.appendChild(submit);
+    submit.addEventListener("click", e);
+    function e() {
+      userInput.textContent = e.data
+      localStorage.setItem("User Name", e.data);
+    }
+    
+    
+  }
 }
 
-function scoreKeeper () {
-    if ()
-}
-
-//Question 1
-
-
-submit.addEventListener("click", function);
-
-
-//Question 2
-
-displayQuestions("question2");
-
-//Question 3
-
-displayQuestions("question3");
-
-//Question 4
-
-displayQuestions("question4");
-
-//Question 5
-
-displayQuestions("question5");
-
-//End Quiz
-
-//Save initials
-
-//Input for initials with submit button
-
-nameInput.setAttribute("type", "text");
 
 //Display high score with initials
